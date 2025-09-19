b1c7c2a4e6558adc8a06f9af73246bd3b838714b
Tharak-2005 <tharakbf@gmail.com>
2025-09-19 17:04:01 +0530

    Frontend

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,9 +1,15 @@
-function Home(){
-    return(
-    <div>
-        <h2>Welcome to BlogVerse</h2>
-        <p>Discover amazing stories, insights, and ideas from our community of writers.Sign in to create your own posts and save your favorites.</p>
-    </div>
+function Home() {
+    const posts=[{
+        
+    }]
+    return (
+        <div>
+            <div className="flex  flex-col justify-center items-center mt-5">
+                <h2 className="font-bold text-3xl">Welcome to BlogVerse</h2>
+                <p className="flex flex-col items-center text-lg text-green-400 w-150">Discover amazing stories, insights, and ideas from our community of <span className="flex justify-center">writers.Sign in to create your own posts and save your favorites.</span></p>
+
+            </div>
+        </div>
     )
 }
 export default Home;
